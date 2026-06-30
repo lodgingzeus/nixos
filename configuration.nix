dca9b5283070b1d6bac15d25016f2b692377cd2e
lodgingzeus <bhagatd585@gmail.com>
2026-06-30 11:50:50 +0530

    nh tool

--- configuration.nix
+++ configuration.nix
@@ -277,6 +277,13 @@
 
    programs.fuse.userAllowOther = true;
 
+   programs.nh = {
+     enable = true;
+     clean.enable = true;
+     clean.extraArgs = "--keep-since 4d --keep 3";
+     flake = "/home/deepak/nixos";
+   };
+
    programs.nix-ld.enable = true;
    programs.nix-ld.libraries = with pkgs; [
     # Add common libraries extensions need (Claude Code needs basic glibc/gcc stuff)
