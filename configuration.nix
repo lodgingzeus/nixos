35560ff4d9b6051832ee6641807f73427cbd03f9
lodgingzeus <bhagatd585@gmail.com>
2026-07-10 15:30:17 +0530

    weekly update

--- configuration.nix
+++ configuration.nix
@@ -308,7 +308,14 @@
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = false;
+      KbdInteractiveAuthentication = false;
+      PermitRootLogin = "no";
+    };
+  };
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
